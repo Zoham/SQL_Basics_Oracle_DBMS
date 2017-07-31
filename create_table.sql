4bfe4b6dd6d1e55dd9080a061fdeb791b61c5c6e
Soham Kulkarni <sohamk97@gmail.com>
2017-07-31 17:34:18 +0530

    SQL Basic Commands

---
+++ create_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Student(
+	Name VARCHAR(30) NOT NULL,
+	Student_number INTEGER NOT NULL,
+	Class INTEGER NOT NULL,
+	Major VARCHAR(10) NOT NULL,
+	PRIMARY KEY (Student_number));
---
+++ insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO Student VALUES ('Smith', 17, 1, 'CS');
+INSERT INTO Student VALUES ('Brown', 8, 2, 'CS');
